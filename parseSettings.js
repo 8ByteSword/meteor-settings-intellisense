3d4998c4351415e8883ca6c24461accea682b86e
fferriols <fferriols.etraid@grupoetra.com>
2024-06-05 16:24:32 +0200

    Click + ctrl functionality

--- parseSettings.js
+++ parseSettings.js
@@ -18,6 +18,29 @@ function readSettings(baseDir, settingsFilePath) {
   return JSON.parse(settings);
 }
 
+/**
+ * Reads and parses the settings.json file.
+ * @param {string} baseDir - The base directory where settings.json is located.
+ * @param {string} settingsFilePath - The custom path to the settings file.
+ * @param {string} setting - The setting to look for.
+ * @returns {Array} - The line and the position in the line respectively.
+ */
+function getSettingPosition(baseDir, settingsFilePath, setting) {
+  let res = [];
+  const filePath = path.isAbsolute(settingsFilePath)
+    ? settingsFilePath
+    : path.join(baseDir, settingsFilePath);
+  if (!fs.existsSync(filePath)) {
+    return res;
+  }
+  const settings = fs.readFileSync(filePath, 'utf8').split(/\r?\n/);
+  settings.forEach((line, index) => {
+    const pos = line.indexOf(setting);
+    if (pos && pos >= 0) res = [index, pos];
+  });
+  return res;
+}
 module.exports = {
   readSettings,
+  getSettingPosition,
 };
